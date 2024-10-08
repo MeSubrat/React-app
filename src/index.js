721bdcec6b9f24df99dfc29fac5d56994421da89
pitambarsahoo86 <pitambar.bs@gmail.com>
2024-10-08 11:06:18 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+//First Project
 const element=<h1>This is a headinG!!</h1>
 ReactDOM.render(element,document.getElementById("root"));
 
